aa85f7ed48a5ea833e407312aa0147ba6937d336
Denis Kabistan <dkabistan@gmail.com>
2014-08-16 16:11:32 -0700

    Changed app directory for express to read from.

--- server/web.js
+++ server/web.js
@@ -1,8 +1,8 @@
-var express = require('express')
+var express = require('express');
 var app = express();
 
-app.set('port',(process.env.PORT || 5000))
-app.use(express.static(__dirname + '/app'))
+app.set('port',(process.env.PORT || 5000));
+app.use(express.static(__dirname + '/../app'));
 
 app.listen(app.get('port'),function(){
   console.log('Express running on localhost:' + app.get('port'))
