3d9b22376936a2eba8bf62e88d153b74d96a4933
Denis Kabistan <dkabistan@gmail.com>
2014-08-16 19:26:41 -0700

    Changed grunt command to build.

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,5 +42,5 @@ module.exports = function(grunt){
   grunt.loadNpmTasks('grunt-nodemon');
   grunt.loadNpmTasks('grunt-concurrent');
 
-  grunt.registerTask('dev',['concurrent:dev']);
+  grunt.registerTask('build',['concurrent:dev']);
 };
